3193e51c25b5935222cc6dd1ea17238e7aaff1b1
yerin <w2006@e-mirim.hs.kr>
2021-10-01 15:38:54 +0900

    load text, with

--- file handler/load_text.py
+++ file handler/load_text.py
@@ -2,9 +2,11 @@ print('전체 한꺼번에 읽기')
 f = open('text.txt', 'r', encoding='utf-8')
 
 data = f.read()
-
 f.close()
+print(data)
 
+with open('text.txt', 'r', encoding='utf-8')as f:
+    data = f.read()
 print(data)
 
 print('한줄 씩 읽기')
